74d02fa10dab0a40cf6b5e4104e117cb77d27630
RachelChen <iceage91@gmail.com>
2021-06-19 10:52:48 +0800

    fix: errorHandler modify response 500

--- tools/errorHandler.js
+++ tools/errorHandler.js
@@ -4,7 +4,7 @@ function errorHandler(err, req, res, next) {
   if (err instanceof ApiErrors) {
     return res.status(err.code).jsonp([{ code: err.code, message: err.msg }])
   }
-  return res.status(500).jsonp([{ code: err.code, message: err.msg }])
+  return res.status(500).jsonp([{ code: 500, message: 'Something went wrong' }])
 }
 
 module.exports = errorHandler
